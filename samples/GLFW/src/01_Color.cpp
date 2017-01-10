bc55c7e56d2c63410da5a4ee5ac135b1b6a09b3e
Hai Nguyen <chaoticbob@me.com>
2017-01-10 05:40:30 -0800

    Corrected asset path

--- samples/GLFW/src/01_Color.cpp
+++ samples/GLFW/src/01_Color.cpp
@@ -143,13 +143,13 @@ void init_tiny_renderer(GLFWwindow* window)
 	tr_create_cmd_n(m_cmd_pool, false, kImageCount, &m_cmds);
 	
 #if defined(TINY_RENDERER_VK)
-	auto vert = load_file("../assets/color_vert.spv");
-	auto frag = load_file("../assets/color_frag.spv");
+	auto vert = load_file("../../assets/color_vert.spv");
+	auto frag = load_file("../../assets/color_frag.spv");
 	tr_create_shader_program(m_renderer, 
 		                     vert.size(), (uint32_t*)(vert.data()), "main", 
 		                     frag.size(), (uint32_t*)(frag.data()), "main", &m_shader);
 #elif defined(TINY_RENDERER_DX)
-	auto hlsl = load_file("../assets/color.hlsl");
+	auto hlsl = load_file("../../assets/color.hlsl");
 	tr_create_shader_program(m_renderer, 
                              hlsl.size(), hlsl.data(), "VSMain", 
 		                     hlsl.size(), hlsl.data(), "PSMain", &m_shader);
