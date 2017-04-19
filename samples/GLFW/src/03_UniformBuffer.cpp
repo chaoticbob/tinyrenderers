8e3fa5d7ecb75f2f84c803a1f2249b2a058e4462
Hai Nguyen <haing@google.com>
2017-04-19 13:41:15 -0700

    Updated path for samples

--- samples/GLFW/src/03_UniformBuffer.cpp
+++ samples/GLFW/src/03_UniformBuffer.cpp
@@ -102,21 +102,21 @@ void init_tiny_renderer(GLFWwindow* window)
 {
     std::vector<const char*> instance_layers = {
 #if defined(_DEBUG)
-        "VK_LAYER_LUNARG_api_dump",
+        //"VK_LAYER_LUNARG_api_dump",
         "VK_LAYER_LUNARG_core_validation",
-        "VK_LAYER_LUNARG_swapchain",
-        "VK_LAYER_LUNARG_image",
-        "VK_LAYER_LUNARG_parameter_validation"
+        //"VK_LAYER_LUNARG_swapchain",
+        //"VK_LAYER_LUNARG_image",
+        //"VK_LAYER_LUNARG_parameter_validation"
 #endif
     };
 
     std::vector<const char*> device_layers = {
 #if defined(_DEBUG)
-        "VK_LAYER_LUNARG_api_dump",
-        "VK_LAYER_LUNARG_core_validation",
-        "VK_LAYER_LUNARG_swapchain",
-        "VK_LAYER_LUNARG_image",
-        "VK_LAYER_LUNARG_parameter_validation"
+        //"VK_LAYER_LUNARG_api_dump",
+        //"VK_LAYER_LUNARG_core_validation",
+        //"VK_LAYER_LUNARG_swapchain",
+        //"VK_LAYER_LUNARG_image",
+        //"VK_LAYER_LUNARG_parameter_validation"
 #endif
     };
 
@@ -298,6 +298,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(640, 480, "03_UniformBuffer", NULL, NULL);
     init_tiny_renderer(window);
 
